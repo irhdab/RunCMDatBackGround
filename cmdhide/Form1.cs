220f81073deb2e2af6d9618a099b81d6bf0c979f
Programmers57 <hanjinkim@protonmail.com>
2024-02-20 14:37:55 +0900

    Add files via upload

---
+++ cmdhide/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+
+namespace cmdhide
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string strCmdText;
+            strCmdText = "taskmgr";
+            System.Diagnostics.Process.Start("CMD.exe", strCmdText);
+        }
+    }
+}
